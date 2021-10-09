9f9c50dac21c7f82d5d6e6b9814116d5f6715716
Uttam Kumar <uttam.kr.work@gmail.com>
2021-10-09 11:10:18 +0530

    commit 7

---
+++ src/main/java/com/xoriant/springbootapp/model/ATestModel.java
@@ -0,0 +1,59 @@
+//for testing purpose
+
+//package com.xoriant.springbootapp.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//
+//@Entity
+//public class ATestModel {
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	private int employeeId;
+//	private String employeeName;
+//	private double salary;
+//	private String address;
+//	private String photoUrl;
+//	private String department;
+//	public int getEmployeeId() {
+//		return employeeId;
+//	}
+//	public void setEmployeeId(int employeeId) {
+//		this.employeeId = employeeId;
+//	}
+//	public String getEmployeeName() {
+//		return employeeName;
+//	}
+//	public void setEmployeeName(String employeeName) {
+//		this.employeeName = employeeName;
+//	}
+//	public double getSalary() {
+//		return salary;
+//	}
+//	public void setSalary(double salary) {
+//		this.salary = salary;
+//	}
+//	public String getAddress() {
+//		return address;
+//	}
+//	public void setAddress(String address) {
+//		this.address = address;
+//	}
+//	public String getPhotoUrl() {
+//		return photoUrl;
+//	}
+//	public void setPhotoUrl(String photoUrl) {
+//		this.photoUrl = photoUrl;
+//	}
+//	public String getDepartment() {
+//		return department;
+//	}
+//	public void setDepartment(String department) {
+//		this.department = department;
+//	}
+//	
+//	
+//}
